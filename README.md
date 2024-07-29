cf025ed18583275cdd9881c98064cef663b3cdc4
LouisAlbanZiko <louisalbanziko1610@gmail.com>
2024-07-29 21:14:10 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# AES Implementation in Rust
+
+Implementation of the AES encryption standard as described here: [FIPS 197, Advanced Encryption Standard (AES)](https://csrc.nist.gov/files/pubs/fips/197/final/docs/fips-197.pdf)
+
+
+### DO NOT USE THIS IN YOUR CODE
+
+I did this as a learning exercise.
+
+Although I did try to follow the specification as closely as I could and I did test with the examples given, I haven't tested it extensively nor is it easy to use because I only implemented the encrypt/decrypt for blocks. You would need to implement the mode of operation yourself or use another library for it.
---
+++ src/aes_128.rs
@@ -0,0 +1,81 @@
+use super::common::*;
+
+const KEY_LENGTH: usize = 4;
+const ROUNDS: usize = 10;
+const KEY_COUNT: usize = (ROUNDS + 1) * 4 / KEY_LENGTH + (((ROUNDS + 1) * 4) % KEY_LENGTH != 0) as usize;
+
+type Key = [u8; 4 * KEY_LENGTH];
+type KeySchedule = [Block; ROUNDS + 1];
+
+pub fn encrypt_block(input: Block, key: Key) -> Block {
+	let w = key_expansion(key);
+
+	let mut state: Block = input;
+
+	state = add_round_key(state, &w[0]);
+
+	for i in 1..ROUNDS {
+		state = sub_bytes(state);
+		state = shift_rows(state);
+		state = mix_columns(state);
+		state = add_round_key(state, &w[i]);
+	}
+
+	state = sub_bytes(state);
+	state = shift_rows(state);
+	state = add_round_key(state, &w[ROUNDS]);
+
+	state
+}
+
+pub fn decrypt_block(input: Block, key: Key) -> Block {
+	let w = key_expansion(key);
+
+	let mut state: Block = input;
+
+	state = add_round_key(state, &w[ROUNDS]);
+
+	let mut r = ROUNDS - 1;
+	while r > 0 {
+		state = inv_shift_rows(state);
+		state = inv_sub_bytes(state);
+		state = add_round_key(state, &w[r]);
+		state = inv_mix_columns(state);
+
+		r = r - 1;
+	}
+
+	state = inv_shift_rows(state);
+	state = inv_sub_bytes(state);
+	state = add_round_key(state, &w[0]);
+
+	state
+}
+
+fn key_expansion(key: Key) -> KeySchedule {
+	let mut w = [[0; 4 * KEY_LENGTH]; KEY_COUNT];
+
+	w[0] = key;
+
+	let mut temp: [u8; 4] = as_2d(&w[0])[3];
+	for r in 1..KEY_COUNT {
+		let word = sub_word(rot_word(temp));
+		temp[0] = word[0] ^ R_CON[r];
+		temp[1] = word[1] ^ 0x00;
+		temp[2] = word[2] ^ 0x00;
+		temp[3] = word[3] ^ 0x00;
+		for k in 0..KEY_LENGTH {
+			for x in 0..4 {
+				w[r][x + k * 4] = w[r - 1][x + k * 4] ^ temp[x];
+			}
+			temp = as_2d(&w[r])[k];
+		}
+	}
+
+	w
+}
+
+fn as_2d(key: &Key) -> &[[u8; 4]; KEY_LENGTH] {
+	unsafe { &*(key.as_ptr() as *const [[u8; 4]; KEY_LENGTH]) }
+}
+
